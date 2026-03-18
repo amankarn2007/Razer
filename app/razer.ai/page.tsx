736beab04f5bb81ec878633fa7345f12f72152bf
Aman Karn <amankarn2007@gmail.com>
2026-03-18 21:52:50 +0530

    razer ai page completed

--- app/razer.ai/page.tsx
+++ app/razer.ai/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import AICards from "../components/razerAi/AICards";
 import Hero from "../components/razerAi/Hero";
 import AINav from "../components/razerAi/navbar";
@@ -15,10 +16,22 @@ export default function RazerAi() {
                 <Hero />
                 <AICards />
                 <NewSec />
-
             </main>
 
-            <footer></footer>
+            <footer>
+                <Footer />
+            </footer>
+
+            <LeftFixedDiv />
+        </div>
+    )
+}
+
+function LeftFixedDiv() {
+    return (
+        <div className="fixed flex flex-col gap-3 px-2 py-5 rounded-r-2xl bg-green-400 top-[40%] left-0">
+            <div className=" py-2 px-2.5 rounded-4xl border-2"><i className="fa-brands fa-linkedin-in"></i></div>
+            <div className=" py-2 px-2.5 rounded-4xl border-2"><i className="fa-brands fa-x-twitter"></i></div>
         </div>
     )
 }
